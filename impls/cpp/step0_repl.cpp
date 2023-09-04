35b6cb4f3e11455d97041fb3a6b819fc161a64d2
cnfloss <jsdevelopercolin@gmail.com>
2023-09-04 06:26:45 -0400

    add git ignore

--- impls/cpp/step0_repl.cpp
+++ impls/cpp/step0_repl.cpp
@@ -1,3 +1,24 @@
 //
 // Created by colin on 9/4/23.
 //
+#include <iostream>
+#include <string>
+using namespace std;
+
+string READ(string input) { return input; }
+string EVAL(string input) { return input; }
+string PRINT(string input) { return input; }
+string rep(string input) {
+  string first = READ(input);
+  string second = EVAL(first);
+  return PRINT(second);
+}
+
+int main() {
+  string input;
+  for(;;) {
+    cout << "User> ";
+    getline(cin, input);
+    cout << rep(input) << endl;
+  }
+}
